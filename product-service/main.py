7eb4f7cd09573c971f4c9ca4f951f0d72ee849bd
sally9476 <sally9476@wisenut.co.kr>
2023-01-05 10:55:12 +0900

    fix: add eureka client

--- product-service/main.py
+++ product-service/main.py
@@ -5,7 +5,14 @@ from pydantic import ValidationError
 
 from routers.product import product_router
 from app.database import conn
+import py_eureka_client.eureka_client as eureka_client
 
+product_service_port = 8084
+eureka_host = "admin:admin@127.0.0.1:8761"
+eureka_client.init(eureka_server=f"http://{eureka_host}/eureka/",
+                   app_name="product-service",
+                   instance_host="127.0.0.1",
+                   instance_port=product_service_port)
 app = FastAPI(
     title='PriceFlow Product Service',
     description='PriceFlow Product Service API',
